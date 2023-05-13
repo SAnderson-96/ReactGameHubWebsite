377ce64fa242845f924251443f7720fed5f4d48a
Samuel Anderson <samuel.c.anderson96@gmail.com>
2023-05-12 21:23:47 -0400

    a little bit before last commit

--- react-game-app/src/App.tsx
+++ react-game-app/src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
+import NavBar from "./components/NavBar";
+import Cart from "./components/CArt";
 
 export default function App() {
   let items = ["New York", "Montreal", "Toronto", "Tokyo", "Seoul"];
@@ -22,8 +24,34 @@ export default function App() {
   };
   const handleButtonClick = () => {};
 
+  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
+
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "John",
+    },
+  });
+
+  const names = ["John", "Bob", "Sam", "Jeff", "Billy", "Joe", "Hoe"];
+
+  const handlePlayerNameChange = () => {
+    let newGame = { ...game, ...game.player };
+    newGame.id = game.id + 1;
+    let chance = Math.floor(Math.random() * 6);
+    newGame.player.name = names[chance];
+    setGame(newGame);
+  };
+
   return (
     <div>
+      <NavBar cartItemsCount={cartItems.length} />
+      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
+      <div>
+        <h1>GameId: {game.id}</h1>
+        <h2>Player Name: {game.player.name}</h2>
+        <button onClick={handlePlayerNameChange}>Click to change name</button>
+      </div>
       <ListGroup
         items={items}
         heading="Cities"
